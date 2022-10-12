5317bda559632d323701a65b660c8d9773427649
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-10-12 19:09:23 +0200

    added normalize to vector and others

--- CVector.cpp
+++ CVector.cpp
@@ -31,7 +31,7 @@ CVector* CVector::Add(CVector& _rOther){
 
     for (int i = 0; i < this->s_Dimensions; i++) {
         
-        result[i] = this->m_Values[i] + _rOther[i]
+        result[i] = this->m_Values[i] + _rOther[i];
         
     }
 
@@ -40,8 +40,7 @@ CVector* CVector::Add(CVector& _rOther){
 
 void CVector::AddAndSet(CVector& _rOther){
 
-        CVector* temp = this->Add(_rOther);
-        this->m_Values = temp->Get();
+        this->Set(this->Add(_rOther));
 
 };
 
@@ -51,7 +50,7 @@ CVector* CVector::Sub(CVector& _rOther){
 
     for (int i = 0; i < this->s_Dimensions; i++) {
         
-        result[i] = this->m_Values[i] - _rOther[i]
+        result[i] = this->m_Values[i] - _rOther[i];
         
     }
 
@@ -60,10 +59,61 @@ CVector* CVector::Sub(CVector& _rOther){
 
 void CVector::SubAndSet(CVector& _rOther){
 
-        CVector* temp = this->Sub(_rOther);
-        this->m_Values = temp->Get();
+        this->Set(this->Sub(_rOther));
         
 };
+
+CVector* CVector::Mul(float _Value){
+
+    float result[this->s_Dimensions];
+
+    for (int i = 0; i < this->s_Dimensions; i++) {
+        
+        result[i] = this->m_Values[i] * _Value;
+        
+    }
+
+    return new CVector(result);
+};
+
+void CVector::MulAndSet(float _Value){
+
+    this->Set(this->Mul(_rOther));
+        
+};
+
+float CVector::Dot3(CVector& _rOther){
+
+    float result;
+
+    for (int i = 0; i < 3; i++) {
+        
+        result =+ this->m_Values[i] * _rOther->Get()[i];
+        
+    }
+
+    return result;
+    
+};
+
+CVector* CVector::Cross3(CVector& _rOther){
+
+    float result[3];
+
+    result[0] = this->m_Values[1] * _rOther->Get()[2] - this->m_Values[2] * _rOther->Get()[1];
+    result[1] = this->m_Values[2] * _rOther->Get()[0] - this->m_Values[0] * _rOther->Get()[2];
+    result[2] = this->m_Values[0] * _rOther->Get()[1] - this->m_Values[1] * _rOther->Get()[0];
+
+    return new CVector(result);
+
+};
+
+void CVector::Cross3AndSet(CVector& _rOther){
+
+    this->Set(this->Cross3(_rOther));
+        
+};
+
 
 
 float CVector::GetLength() const{
@@ -77,6 +127,18 @@ float CVector::GetLength() const{
     }
 
     return sqrt(temp);
+
+};
+
+CVector* CVector::GetNormalized(){
+
+    return this->Mul( 1 /  ( this->GetLength() ));
+
+};
+
+void CVector::Normalize(){
+
+    this->Set(this->GetNormalized());
 
 };
 
--- CVector.h
+++ CVector.h
@@ -18,6 +18,18 @@ class CVector{
         CVector* Sub(CVector& _rOther);
         void SubAndSet(CVector& _rOther);
 
+        CVector* Mul(float _Value);
+        void MulAndSet(float _Value);
+
+        float Dot3(CVector& _rOther);
+        CVector* Cross3(CVector& _rOther);
+        void Cross3AndSet(CVector& _rOther);
+        
+        CVector* GetNormalized();
+        void Normalize();
+
+        
+
         float GetLength() const;
         CVector* GetCross(const CVector& _rOther) const;
 
