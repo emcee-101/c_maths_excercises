f87072cacbaa89af7e1623652a7b87a7e6025deb
Niklas Herzog <nik.las.herzog@t-online.de>
2022-10-13 16:22:38 +0200

    working in vs

--- root/heron.cpp
+++ root/heron.cpp
@@ -1,8 +1,8 @@
-#include "heron.h"
+#include "heron.hpp"
 #include <iostream>
 #include <cmath>
 
-float heron(int number, int grade){
+float heron(int number, int grade) {
 
 	float result = 0.0f;
 	float x = 1.0f;
@@ -11,14 +11,14 @@ float heron(int number, int grade){
 	float cur_accuracy = 1000.0f;
 	std::cout << "\n";
 
-	while(cur_accuracy > desired_accuracy){
-		
+	while (cur_accuracy > desired_accuracy) {
+
 		count++;
 
-		result = ((grade - 1) * pow(x, grade) + number) / (grade * pow (x, grade - 1));
-		
-		std::cout << "Step "<< count << ": " << result << "\n";
-		
+		result = ((grade - 1) * pow(x, grade) + number) / (grade * pow(x, grade - 1));
+
+		std::cout << "Step " << count << ": " << result << "\n";
+
 		cur_accuracy = std::abs(x - result);
 		x = result;
 
--- root/main.cpp
+++ root/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "heron.h"
+#include "heron.hpp"
 
 
 int main(char* args[]) {
@@ -7,18 +7,18 @@ int main(char* args[]) {
 	std::cout << "Greetings!!! \n";
 
 	std::cout << "What is the number you want to know the root of? \n";
-	
+
 	int a = 0;
 	std::cin >> a;
 
 	std::cout << "What is the grade of the root you wanna know? \n";
-	
+
 	int n = 0;
 	std::cin >> n;
 
 	float result = heron(a, n);
 	std::cout << "\nYour result is: " << result << "! :)";
-	
+
 	return 0;
 
 }
